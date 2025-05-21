cda34363d7e83f61112e59678c73d7c54d751555
Cláudio Barroso <109551603+aubaro01@users.noreply.github.com>
2025-05-21 16:06:13 +0100

    fix
    fix deploy error

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,24 @@
-const express = require('express');
-const serverless = require('serverless-http');
-const cors = require('cors');
-const apodRoutes = require('./Routes/apodRoute');
 require('dotenv').config();
-
+const axios = require('axios');
+const express = require('express');
 const app = express();
 
-app.use(cors());
-app.use('/api', apodRoutes);
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
-module.exports = serverless(app);
+app.get('/api/apod', async (req, res) => {
+  try {
+    const response = await axios.get('https://api.nasa.gov/planetary/apod', {
+      params: { api_key: process.env.NASA_API_KEY }
+    });
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao buscar dados da NASA: ' + error.message });
+  }
+});
+
+module.exports = app;
